330f07a8a0a94252025d12328f8a0df16aa4cb49
hirsceva <hirsceva@students.zhaw.ch>
2021-12-12 13:21:24 +0100

    Added UpdateDelayTimer to LocalApp

--- LocalAppNew/Form1.Designer.cs
+++ LocalAppNew/Form1.Designer.cs
@@ -29,26 +29,44 @@ namespace LocalAppNew
         /// </summary>
         private void InitializeComponent()
         {
+            this.updateDelayText = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // updateDelayText
+            // 
+            this.updateDelayText.AutoSize = true;
+            this.updateDelayText.ForeColor = System.Drawing.Color.Red;
+            this.updateDelayText.Location = new System.Drawing.Point(12, 9);
+            this.updateDelayText.Name = "updateDelayText";
+            this.updateDelayText.Size = new System.Drawing.Size(32, 15);
+            this.updateDelayText.TabIndex = 1;
+            this.updateDelayText.Text = "0 ms";
+            this.updateDelayText.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(777, 778);
+            this.ClientSize = new System.Drawing.Size(680, 584);
+            this.Controls.Add(this.updateDelayText);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Practice";
             this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label updateDelayText;
     }
 }
 
